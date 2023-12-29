d3fc5789f2770f8e6e83db41bfa5c49f830c4f34
ramzhyz <131998972+ramzhyz@users.noreply.github.com>
2023-12-30 02:42:36 +0800

    Update pilihanhotel.php

--- app/Views/main/pilihanhotel.php
+++ app/Views/main/pilihanhotel.php
@@ -58,7 +58,7 @@
                   <div class="col-lg">
                     <div class="card-deck">
                       <div class="card" href="#">
-                        <img class="card-img-top" src="img/box1.jpg" alt="Card image cap">
+                        <img class="card-img-top" src="assets/main/img/box1.jpg" alt="Card image cap">
                         <div class="card-body">
                           <h5 class="card-title">Hotel Contoh</h5>
                           <p class="card-text">Deskripsi Hotel.</ps>
@@ -66,7 +66,7 @@
                         </div>
                       </div>
                       <div class="card" href="#">
-                      <img class="card-img-top" src="img/box1.jpg" alt="Card image cap">
+                      <img class="card-img-top" src="assets/main/img/box1.jpg" alt="Card image cap">
                         <div class="card-body">
                           <h5 class="card-title">Hotel Contoh</h5>
                           <p class="card-text">Deskripsi Hotel.</ps>
@@ -74,7 +74,7 @@
                         </div>
                       </div>
                       <div class="card" href="#">
-                      <img class="card-img-top" src="img/box1.jpg" alt="Card image cap">
+                      <img class="card-img-top" src="assets/main/img/box1.jpg" alt="Card image cap">
                         <div class="card-body">
                           <h5 class="card-title">Hotel Contoh</h5>
                           <p class="card-text">Deskripsi Hotel.</ps>
